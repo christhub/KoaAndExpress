1e1472137d767e3e7675da28f317cec6918e9310
Christian <christian.lei1@gmail.com>
2015-11-11 23:26:54 -0800

    add routing for a tour page

--- meadowlark.js
+++ meadowlark.js
@@ -15,13 +15,31 @@ app.set('port' , process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(function(req, res, next){
+        res.locals.showTests = app.get('env') !== 'production' &&
+                  req.query.test == '1';
+        next();
+});
+
 app.get('/', function(req, res){
         res.render('home');
 });
 
 app.get('/about', function(req, res){
-        res.render('about', { bridgeFact: bridgeFact.getBridgeFact() });
+        res.render('about', {
+          bridgeFact: bridgeFact.getBridgeFact(),
+          pageTestScript: '/qa/tests-about.js'
+        } );
 });
+
+//bridge tour page
+  app.get('/tours/hood-river', function(req, res){
+          res.render('tours/hood-river');
+  });
+  app.get('/tours/request-group-rate', function(req, res){
+          res.render('tours/request-group-rate');
+  });
+  
 //custom 404 page
 app.use(function(req, res){
         res.status(404);
