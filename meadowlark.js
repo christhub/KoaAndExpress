e5c96142088be39bdc37452c1cafcb79c0f6c779
Christian <christian.lei1@gmail.com>
2015-11-11 22:13:47 -0800

    a small express website that has a random bridge fact

--- meadowlark.js
+++ meadowlark.js
@@ -2,31 +2,46 @@ var express = require('express');
 
 var app = express();
 
+var bridgeFacts = [
+  'the bridge is getting you over the river',
+  'there is the marquam bridge that carries I-5 over the willamette',
+  'the fremont bridge carries I-405 over the willamette',
+  'the broadway bridge carries broadway over the willamette',
+  'the ross island bridge carries 26 over the willamette river',
+  'the sellwood bridge connects sellwood to the other side of the river'
+]
+
+// //set-up jade
+// app.engine('.html', require('jade'));
+
+//set up handlebars view engine
+var handlebars = require('express-handlebars').create({ defaultLayout:'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
 app.set('port' , process.env.PORT || 3000);
 
-// custom 404 page
-app.get('/', function(req,res){
-        res.type('text/plain');
-        res.send('Meadowlark Travel');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+        res.render('home');
 });
 
 app.get('/about', function(req, res){
-        res.type('text/plain');
-        res.send('About Meadowlark Travel')
+        var randomBridgeFact =
+                bridgeFacts[Math.floor(Math.random() * bridgeFacts.length)];
+        res.render('about', { bridgeFact: randomBridgeFact });
 });
-
+//custom 404 page
 app.use(function(req, res){
-        res.type('text/plain');
         res.status(404);
-        res.send('404 - Not Found Bigga');
+        res.render('404');
 });
 
-// custom 500 page
+// custom 500 handler (middleware)
 app.use(function(err, req, res, next){
-        console.error(err.stack);
-        res.type('text/plain');
         res.status(500);
-        res.send('500 - Server Error');
+        res.render('500');
 });
 
 app.listen(app.get('port'), function(){
