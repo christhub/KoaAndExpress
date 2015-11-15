89f919c84eb275111d8becb39329635efbc8e060
Christian <christian.lei1@gmail.com>
2015-11-15 10:48:30 -0800

    file uploads and sessions

--- meadowlark.js
+++ meadowlark.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var formidable = require('formidable');
+var credentials = require('./credentials.js');
 var bridgeFact = require('./lib/bridgeFact.js');
 var weatherData = require('./lib/weatherData.js');
 
@@ -24,13 +26,40 @@ app.set('view engine', 'handlebars');
 
 app.set('port' , process.env.PORT || 3000);
 
+
+app.use(require('cookie-parser')(credentials.cookieSecret));
+app.use(require('express-session')({
+    resave: false,
+    saveUninitialized: false,
+    secret: credentials.cookieSecret,
+}));
 app.use(express.static(__dirname + '/public'));
 app.use(require('body-parser')());
+
+app.use(function(req, res, next){
+	// if there's a flash message, transfer
+	// it to the context, then clear it
+	res.locals.flash = req.session.flash;
+	delete req.session.flash;
+	next();
+});
 
 app.use(function(req, res, next){
       res.locals.showTests = app.get('env') !== 'production' &&
                 req.query.test == '1';
       next();
+});
+
+app.use('/upload', function(req, res, next){
+  var now = Date.now();
+  jqupload.fileHandler({
+    uploadDir: function(){
+      return __dirname + 'public/uploads/' + now;
+    },
+    uploadUrl: function(){
+      return '/uploads/' + now;
+    },
+  })(req, res, next);
 });
 
 // Partials & weather data
@@ -72,10 +101,10 @@ app.use(function(req, res, next){
 
 
 app.get('/headers', function(req, res){
-res.set('Content-Type', 'text/plain');
-var s = '';
-for(var name in req.headers) s += name + ': ' + req.headers[name] + '\n';
-res.send(s);
+  res.set('Content-Type', 'text/plain');
+  var s = '';
+  for(var name in req.headers) s += name + ': ' + req.headers[name] + '\n';
+  res.send(s);
 });
 
 app.get('/', function(req, res){
@@ -122,25 +151,87 @@ app.get('/data/nursery-rhyme', function(req, res){
 });
 
 app.get('/newsletter', function(req, res){
-  res.render('newsletter', {csrf: 'CSRF token goes here'});
+  res.render('newsletter');
 });
 
 app.get('/thank-you', function(req, res){
   res.render('thank-you');
 });
 
+//file uploads
+app.get('/contest/vacation-photo', function(req, res){
+  var now = new Date();
+  res.render('contest/vacation-photo', {
+    year: now.getFullYear(), month: now.getMonth()
+  });
+});
+
+//post requests
+// for now, we're mocking NewsletterSignup:
+function NewsletterSignup(){
+}
+NewsletterSignup.prototype.save = function(cb){
+	cb();
+};
+
+var VALID_EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/;
+
+app.post('/newsletter', function(req, res){
+	var name = req.body.name || '', email = req.body.email || '';
+	// input validation
+	if(!email.match(VALID_EMAIL_REGEX)) {
+		if(req.xhr) return res.json({ error: 'Invalid name email address.' });
+		req.session.flash = {
+			type: 'danger',
+			intro: 'Validation error!',
+			message: 'The email address you entered was  not valid.',
+		};
+		return res.redirect(303, '/newsletter/archive');
+	}
+	new NewsletterSignup({ name: name, email: email }).save(function(err){
+		if(err) {
+			if(req.xhr) return res.json({ error: 'Database error.' });
+			req.session.flash = {
+				type: 'danger',
+				intro: 'Database error!',
+				message: 'There was a database error; please try again later.',
+			};
+			return res.redirect(303, '/newsletter/archive');
+		}
+		if(req.xhr) return res.json({ success: true });
+		req.session.flash = {
+			type: 'success',
+			intro: 'Thank you!',
+			message: 'You have now been signed up for the newsletter.',
+		};
+		return res.redirect(303, '/newsletter/archive');
+	});
+});
+
+app.get('/newsletter/archive', function(req, res){
+	res.render('newsletter/archive');
+});
+
 app.post('/process', function(req, res){
-  // console.log('form (from querystring): '+ req.query.form);
-  // console.log('CSRF token (from hidden form field): ' + req.body._csrf);
-  // console.log('Name (from visible form field): ' + req.body.name );
-  // console.log('email (from visible form field): ' + req.body.email);
   if(req.xhr || req.accepts('json,html')==='json'){
     res.send({ success: true });
   } else {
     res.redirect(303, '/thank-you');
-    console.log('dk');
   }
 });
+
+app.post('/contest/vacation-photo/:year/:month', function(req, res){
+  var form = new formidable.IncomingForm();
+  form.parse(req, function(err, fields, files){
+    if(err) return res.redirect(303, '/error');
+    console.log('received fields: ');
+    console.log(fields);
+    console.log('received files:');
+    console.log(files);
+    res.redirect(303, '/thank-you');
+  });
+});
+
 
 //custom 404 page
 app.use(function(req, res){
